eb086b6615898c52862caa5e10aa905f7b651126
adityabtambe <139045904+adityabtambe@users.noreply.github.com>
2023-07-09 16:19:15 +0530

    word count example (#1)
    Co-authored-by: bwthadoop <hadoopbwt.batch21@gmail.com>

--- session1/rddwordcount.py
+++ session1/rddwordcount.py
@@ -8,3 +8,18 @@ if __name__ == '__main__':
     spark = SparkSession.builder.master("local[*]").getOrCreate()
 
     # Create RDD using external file
+    input_rdd = spark.sparkContext.textFile(r"D:\BWT Class 2023\sparksession\input_data\inputdata.txt")
+
+    # split rdd using single space, As our file is also having word separated based on spaces
+    split_rdd = input_rdd.flatMap(lambda x: x.split(" "))
+
+    # Attach 1 to each word
+    map_rdd = split_rdd.map(lambda x: (x, 1))
+
+    # get count for each word
+    res_rdd = map_rdd.reduceByKey(lambda x,y: x + y)
+
+    # display result
+    print("Word count example result: ")
+    for element in res_rdd.collect():
+        print(f"{element}")
